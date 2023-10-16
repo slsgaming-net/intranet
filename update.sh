469d0d9db6835ff0d37794ae8dd093cd865f8dd1
dojodetroit <dojodetroit@gmail.com>
2023-10-16 06:39:42 -0400

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-sudo rm -r /var/www/html/intranet -y
+sudo rm -r /var/www/html/intranet 
 echo "Deleting Previous Directory..."
 git clone https://github.com/slsgaming-net/intranet /var/www/html/intranet
 echo "Cloned Repo!"
