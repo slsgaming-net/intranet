5846c79f87e20f3fceb55cd6ac894bb313adb88f
dojodetroit <dojodetroit@gmail.com>
2023-10-16 08:07:00 -0400

    yaaayyyyy

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@ clear
 sudo rm -r /var/www/html/intranet
 echo "Deleting Previous Directory..."
 sudo git clone https://github.com/slsgaming-net/intranet /var/www/html/intranet
-echo "Cloned Repo!"
 sleep 3
 clear
+echo "Cloned Repo!"
