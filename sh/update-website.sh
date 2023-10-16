9de2dde708e1405a41db372d32e3b369eed77311
dojodetroit <dojodetroit@gmail.com>
2023-10-16 08:03:50 -0400

    sh scripts

--- sh/update-website.sh
+++ sh/update-website.sh
@@ -1,8 +1,10 @@
 clear
 echo "Removing update.sh..."
 sudo rm -f update.sh
-echo "Deleted update.sh"
-sleep 5
+sudo rm -r /var/www/html/intranet
+echo "Deleted update.sh and /internet"
+sleep 3
 wget https://raw.githubusercontent.com/slsgaming-net/intranet/main/update.sh
 clear
 sudo sh update.sh
+sleep 3 clear
