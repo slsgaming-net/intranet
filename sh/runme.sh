1cc321516507958e49eb5ef13ad70a5bacfe424c
dojodetroit <dojodetroit@gmail.com>
2024-06-27 02:50:11 -0400

    new menu

--- sh/runme.sh
+++ sh/runme.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-HEIGHT=15
-WIDTH=40
-CHOICE_HEIGHT=4
-BACKTITLE="SLS Menu"
-TITLE="SLS Menu"
-MENU="Choose one of the following options:"
+items=(1 "Update Website"
+       2 "Update SH")
 
-OPTIONS=(1 "Update Website"
-         2 "Setup BYOND Hosting"
-         3 "Exit")
-
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-case $CHOICE in
-        1)
-            echo "Updating Website..."
-			sudo sh update-website.sh
-        2)
-            echo "Coming Soon..."
-        3)
-            esac
-esac
+while choice=$(dialog --title "$TITLE" \
+                 --menu "Please select" 10 40 3 "${items[@]}" \
+                 2>&1 >/dev/tty)
+    do
+    case $choice in
+        1) sudo sh update.sh # some action on 1
+        2) echo "lol"
+        *) ;; # some action on other
+    esac
+done
+clear # clear after user pressed Cancel
