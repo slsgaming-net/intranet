a2b6c12334625673acbaf6bac71a3f596ae99e78
dojodetroit <dojodetroit@gmail.com>
2024-06-27 01:30:11 -0400

    runme.sh

---
+++ sh/README.txt
@@ -0,0 +1,2 @@
+These are bash/sh scripts to be used with ubuntu or debian linux distros!
+documentation coming soon!
---
+++ sh/runme.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="SLS Menu"
+TITLE="SLS Menu"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "Update Website"
+         2 "Setup BYOND Hosting"
+         3 "Exit")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "Updating Website..."
+			sudo sh update-website.sh
+            ;;
+        2)
+            echo "Coming Soon..."
+            ;;
+        3)
+            ;;
+esac
